629fb535a335eef047adea416946fc31770a8933
cgdank <cgdank@gmail.com>
2024-04-11 21:14:30 -0400

    Testing

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Movie from './Movie';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">List of Movies</h1>
+      <div className="row">
+        <div className="col-lg-4">
+          <Movie />
+        </div>
+        <div className="col-lg-4">
+          <Movie />
+        </div>
+        <div className="col-lg-4">
+          <Movie />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Movie() {
+  return (
+    <div className="movie">
+        <div className="movie-title">
+            <b>Movie Title: The Batman</b>
+        </div>
+        <div className="movie-director">
+            Director: Matt Reeves
+        </div>
+        <div className="movie-stars">
+            Stars: Robert Pattinson, Zoe Kravitz
+        </div>
+    </div>
+  )
+}
